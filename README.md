49e3010aa48c40a669445d5aa0917fd2dfd9b31b
april-jiyeon-kim <yeon19april@gmail.com>
2019-10-27 20:28:32 +0900

    feat: Deploy to github pages

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Movie App 2019
 
 React JS Study
+(https://april-jiyeon-kim.github.io/movie_app_2019/)
--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
         data: { movies }
       }
     } = await axios.get(
-      "http://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
     );
     this.setState({ movies, isLoading: false });
   };
@@ -24,24 +24,27 @@ class App extends React.Component {
   render() {
     const { isLoading, movies } = this.state;
     return (
-      <div>
-        {isLoading
-          ? "Loading..."
-          : movies.map(movie => {
-              console.log(movie);
-              return (
-                <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary}
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                />
-              );
-            })}
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+            ))}
+          </div>
+        )}
+      </section>
     );
   }
 }
