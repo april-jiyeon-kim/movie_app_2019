d5c4d1d532ecfc40ccc6b188ff348f93f24dfb25
april-jiyeon-kim <yeon19april@gmail.com>
2019-10-27 20:08:13 +0900

    feat: Add genres

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 class App extends React.Component {
   state = {
@@ -36,6 +37,7 @@ class App extends React.Component {
                   title={movie.title}
                   summary={movie.summary}
                   poster={movie.medium_cover_image}
+                  genres={movie.genres}
                 />
               );
             })}
