84cc6f74d290ab371290788c346429aae35b2e5f
april-jiyeon-kim <yeon19april@gmail.com>
2019-10-27 20:21:30 +0900

    Updates

---
+++ precache-manifest.7b3b92c4134a4292d2ebbbf8be78d363.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fbe3d192d311d70d0bb51a8e1849377",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "e58775f494439d2e0716",
+    "url": "/movie_app_2019/static/css/main.10b0ebdd.chunk.css"
+  },
+  {
+    "revision": "80259c83d9647585c875",
+    "url": "/movie_app_2019/static/js/2.86247d35.chunk.js"
+  },
+  {
+    "revision": "e58775f494439d2e0716",
+    "url": "/movie_app_2019/static/js/main.cd532e6c.chunk.js"
+  },
+  {
+    "revision": "33b05d32a75d3288a422",
+    "url": "/movie_app_2019/static/js/runtime-main.6de02104.js"
+  }
+]);
